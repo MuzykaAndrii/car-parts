f0096d43529672fcda6e7e1bfa70e4c193b045be
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-17 20:28:02 +0200

    impl: adding car to garage feature

--- garage/urls.py
+++ garage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from garage.views import GarageView
+from garage.views import AddCarToGarageView, GarageView
 
 
 app_name = 'garage'
 
 urlpatterns = [
     path("", GarageView.as_view(), name="garage"),
+    path("add/<str:car_vin>", AddCarToGarageView.as_view(), name="add_car"),
 ]
--- garage/views.py
+++ garage/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
+
+from main.models import Auto
 
 
 class GarageView(View):
@@ -8,3 +10,11 @@ class GarageView(View):
         user_cars = request.user.cars.all()
 
         return render(request, "garage/index.html", {"cars": user_cars})
+
+
+class AddCarToGarageView(View):
+    def post(self, request: HttpRequest, car_vin: str):
+        car_to_add = get_object_or_404(Auto, vin=car_vin)
+        request.user.cars.add(car_to_add)
+
+        return redirect("main:car_catalog", car_vin=car_vin)
