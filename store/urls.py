d91de54630662ee71a0788a0cc832e0f5b2d0d44
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-18 22:41:00 +0200

    defined structure of adding to cart feature

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from store.views import AddToOrderView
+
 
 app_name = 'store'
 
 urlpatterns = [
-
+    path("add_to_order/", AddToOrderView.as_view(), name="add_to_order"),
 ]
