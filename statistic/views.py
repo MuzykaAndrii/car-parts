c40aed80128c4955c1b9c0f0d5221dc8f1aecb88
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 19:06:10 +0200

    refactor: statistic view appropriate to new order statuses

--- statistic/views.py
+++ statistic/views.py
@@ -1,9 +1,11 @@
+from itertools import chain
 from typing import Any
 
 from django.views.generic import TemplateView
 
 from main.models import PartUnit
 from auth.mixins import AdminRequiredMixin
+from store.models import Order
 
 class IndexPage(TemplateView, AdminRequiredMixin):
     template_name = 'statistic/index.html'
@@ -11,12 +13,14 @@ class IndexPage(TemplateView, AdminRequiredMixin):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         
-        queryset = PartUnit.objects.all()
+        sold_in_orders = PartUnit.objects.filter(order__status=Order.OrderStatus.RECEIVED)
+        sold_without_orders = PartUnit.objects.filter(order=None)
+        sold_total = tuple(chain(sold_in_orders, sold_without_orders))
         
-        sales = {date: 0 for date in set(map(lambda item: str(item.sale_date.date()), queryset))}
-        for el in queryset:
+        sales = {date: 0 for date in set(map(lambda item: str(item.sale_date.date()), sold_total))}
+        for el in sold_total:
             sales[str(el.sale_date.date())] += el.sell_price
-        margin = sum(map(lambda item: item.sell_price - item.buy_price, queryset))
+        margin = sum(map(lambda item: item.sell_price - item.buy_price, sold_total))
 
         context['sale_date'] = list(sales.keys())
         context['sale_values'] = list(sales.values())
