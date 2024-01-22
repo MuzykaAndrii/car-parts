f639f9645016c966fdd24897fa65c9723584cfce
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 15:30:19 +0200

    impl: app for dividing authentication to separate module

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,16 @@
+import re
+
 from django.urls import include, path, re_path
+from django.views.static import serve
 from django.conf import settings
+
 from main.admin import admin_site
 from main.views import handler500 as h500
-import re
-from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin_site.urls),
+    path('auth/', include('auth.urls'), namespace='auth'),
     path('', include('main.urls', namespace='main')),
     path('stats/', include('statistic.urls', namespace='stats')),
     re_path( r"^%s(?P<path>.*)$" % re.escape(settings.STATIC_URL.lstrip("/")), view=serve, kwargs = {'document_root':settings.STATIC_ROOT}),
