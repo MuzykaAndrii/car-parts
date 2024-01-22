a352bc25683748ab4e3bcc330d821c27d1997b2a
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 16:29:58 +0200

    impl: get handler for settings page

--- user/views.py
+++ user/views.py
@@ -2,12 +2,22 @@ from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
+from auth.mixins import MyLoginRequiredMixin
+
+from user.forms import ShippingAddressForm
+from user.models import ShippingAddress
 
 
-class SettingsView(View):
+class SettingsView(MyLoginRequiredMixin, View):
     def get(self, request: HttpRequest):
-        # Show settings page
-        return render(request, 'user/settings.html')
+        try:
+            shipping_address_instance = ShippingAddress.objects.get(user=request.user)
+        except ShippingAddress.DoesNotExist:
+            shipping_address_instance = None
+
+        shipping_form = ShippingAddressForm(instance=shipping_address_instance)
+
+        return render(request, 'user/settings.html', {"shipping_form": shipping_form})
 
     def post(self, request: HttpRequest):
         # upd account data
