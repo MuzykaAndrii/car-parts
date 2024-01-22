1316d82c93b5b282e3eeef0bae35374d82468ef2
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 15:22:01 +0200

    impl: form for shipping address

--- store/forms.py
+++ store/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from user.models import ShippingAddress
+
+
 class AddToOrderForm(forms.Form):
     part_id = forms.IntegerField(widget=forms.HiddenInput)
     quantity = forms.IntegerField(
@@ -10,3 +13,9 @@ class AddToOrderForm(forms.Form):
 
 class DeleteFromOrderForm(forms.Form):
     part_unit_pk = forms.IntegerField(widget=forms.HiddenInput)
+
+
+class ShippingAddressForm(forms.ModelForm):
+    class Meta:
+        model = ShippingAddress
+        fields = ['first_name', 'last_name', 'phone_number', 'region', 'city', 'office_number']
