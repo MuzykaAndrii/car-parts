67437c3b73132eac449d2af3476f45ed717f6fb7
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 15:01:54 +0200

    impl: shipping address model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShippingAddress(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="shipping_address",
+        verbose_name="Адреса доставки",
+    )
+    first_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="Імя",
+    )
+    last_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="Прізвище",
+    )
+    phone_number = models.CharField(
+        max_length=15,
+        null=False,
+        blank=False,
+        verbose_name="Номер телефону",
+    )
+    region = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="Область",
+    )
+    city = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="Населений пункт",
+    )
+    office_number = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+        verbose_name="Номер відділення",
+    )
