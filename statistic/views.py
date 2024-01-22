d1af1e1d571fd594b43f6157a235d89b53993276
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-23 00:25:31 +0200

    fix: inconsistent date ordering in chart

--- statistic/views.py
+++ statistic/views.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
 from functools import reduce
-from itertools import chain
 from typing import Any
 
 from django.views.generic import TemplateView
+from django.db.models import Q
 
 from main.models import PartUnit
 from auth.mixins import AdminRequiredMixin
@@ -15,16 +15,17 @@ class IndexPage(TemplateView, AdminRequiredMixin):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         
-        sold_with_orders = PartUnit.objects.filter(order__status=Order.OrderStatus.RECEIVED)
-        sold_without_orders = PartUnit.objects.filter(order=None)
-        sold_total = tuple(chain(sold_with_orders, sold_without_orders))
+        # TODO: move sold date to order model instead partunit model and refactor this method appropriately tto new structure
+        sold_product_units = PartUnit.objects.filter(
+            Q(order__status=Order.OrderStatus.RECEIVED) | Q(order=None),
+        ).order_by("sale_date")
 
         sales = defaultdict(float)
-        for sold_product in sold_total:
+        for sold_product in sold_product_units:
             sold_at = str(sold_product.sale_date.date())
             sales[sold_at] += sold_product.sell_price * sold_product.quantity
         
-        margin = reduce(lambda acc, item: acc + (item.sell_price - item.buy_price) * item.quantity, sold_total, 0)
+        margin = reduce(lambda acc, item: acc + (item.sell_price - item.buy_price) * item.quantity, sold_product_units, 0)
 
         context['sale_date'] = list(sales.keys())
         context['sale_values'] = list(sales.values())
