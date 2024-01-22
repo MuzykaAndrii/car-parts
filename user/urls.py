cd9d172662fc3a6e44dacdfe8cb38fc84d955ff7
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-22 15:56:45 +0200

    defined structure of settings page

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from user.views import SettingsView
+
 
 app_name = "user"
 
 urlpatterns = [
-
+    path("settings/", SettingsView.as_view(), name="settings"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login
-from django.contrib.auth import logout
+from django.views import View
 
+
+class SettingsView(View):
+    def get(self, request: HttpRequest):
+        # Show settings page
+        pass
+
+    def post(self, request: HttpRequest):
+        # upd account data
+        pass
