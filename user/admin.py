849055a01ce43160dfcd376fc034fce32ccc7c6b
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-24 16:57:01 +0200

    impl: admin for user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from core.admin import admin_site
+from store.models import Order
+
+
+class OrdersInline(admin.TabularInline):
+    model = Order
+    extra = 0
+    verbose_name = "Замовлення"
+    verbose_name_plural = "Замовлення"
+    fields = ("pk", "status", "sold_at")
+    readonly_fields = ("pk", "sold_at")
+    show_change_link = True
+
+UserAdmin.inlines += (OrdersInline, )
+admin_site.register(User, UserAdmin)
