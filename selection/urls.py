431f865dec2489ed4af0dc96f12afe87506b9240
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-31 18:40:05 +0200

    defined: Selection request view

--- selection/urls.py
+++ selection/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, path
+from django.urls import path
+
+from selection.views import SelectionRequestView
 
 
 app_name = "selection"
 
 urlpatterns = [
-    
+    path("/request_selection", SelectionRequestView.as_view(), name="request_selection"),
 ]
--- selection/views.py
+++ selection/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class SelectionRequestView(View):
+    def get(self, request: HttpRequest):
+        return render(request, "selection/request.html")
