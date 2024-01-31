8ba5daf7598611ece825022ca951798218aca540
MuzykaAndrii <andrymyzik@gmail.com>
2024-01-31 18:52:19 +0200

    impl: selection btn in navbar

--- selection/urls.py
+++ selection/urls.py
@@ -6,5 +6,5 @@ from selection.views import SelectionRequestView
 app_name = "selection"
 
 urlpatterns = [
-    path("/request_selection", SelectionRequestView.as_view(), name="request_selection"),
+    path("request/", SelectionRequestView.as_view(), name="request"),
 ]
