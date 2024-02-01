8faefc8c8247da5f8934dac2f8ab25b2df5c8457
MuzykaAndrii <andrymyzik@gmail.com>
2024-02-01 20:45:25 +0200

    impl: accepting selection feature

--- selection/views.py
+++ selection/views.py
@@ -2,10 +2,13 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.contrib import messages
+from django.db import transaction
 
 from auth.mixins import MyLoginRequiredMixin
+from main.models import PartUnit
 from selection.forms import SelectionRequestForm
 from selection.models import SelectionRequest
+from store.models import Order
 
 
 class SelectionRequestView(MyLoginRequiredMixin, View):
@@ -44,5 +47,24 @@ class RefuseSelectionView(MyLoginRequiredMixin, View):
         return redirect("selection:list")
 
 class AcceptSelectionView(MyLoginRequiredMixin, View):
+    @transaction.atomic
     def post(self, request: HttpRequest, pk: int):
-        ...
+        selection_to_accept = get_object_or_404(SelectionRequest.objects.prefetch_related("response__proposal"), pk=pk, sender=request.user)
+        selection_to_accept.status = SelectionRequest.STATUSES.ACCEPTED
+        selection_to_accept.save()
+
+        user_cart = Order.objects.get_or_create(customer=request.user, status=Order.STATUSES.IN_CART)[0]
+
+        part_units: list = []
+        for part in selection_to_accept.response.proposal.all():  # type: Part
+            part_units.append(PartUnit(
+                part=part,
+                order=user_cart,
+                buy_price=part.buy_price,
+                sell_price=part.sell_price,
+            ))
+
+        PartUnit.objects.bulk_create(part_units)
+
+        messages.success(request, "Підібрані товари успішно перенесені до вашої корзини!")
+        return redirect("store:cart")
