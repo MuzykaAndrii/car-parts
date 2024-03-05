ce0250ba91235073e86e6326f770a1b08e613a12
MuzykaAndrii <andrymyzik@gmail.com>
2024-03-05 23:40:33 +0200

    impl: config from env file

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,15 @@
-from pydantic_settings import BaseSettings
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).resolve().parent
+ENV_FILE_PATH = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=ENV_FILE_PATH, env_file_encoding="utf-8")
+    
     DEBUG: bool
     BOT_TOKEN: str
 
