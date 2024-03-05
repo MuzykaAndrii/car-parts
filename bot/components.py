f03eb8aec4d8a09f27a9a17636ae7a77271e8274
MuzykaAndrii <andrymyzik@gmail.com>
2024-03-05 19:20:11 +0200

    impl: backend service component

--- bot/components.py
+++ bot/components.py
@@ -1,7 +1,9 @@
 from functools import lru_cache
 
 from aiohttp import ClientSession
+
 from sessions import session_factory
+from backend.services import BackendService
 
 
 @lru_cache
@@ -9,3 +11,7 @@ def backend_session() -> ClientSession:
     return session_factory(
         base_url="http://127.0.0.1:8000",
     )
+
+
+def backend_service():
+    return BackendService(backend_session())
