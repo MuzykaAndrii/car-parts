f8c49fc327674710ef625d365d113ef5ece3ff81
MuzykaAndrii <andrymyzik@gmail.com>
2024-03-07 13:57:44 +0200

    impl: telegram account model

--- site/telegram/models.py
+++ site/telegram/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Account(models.Model):
+    id = models.IntegerField(
+        verbose_name='Ідентифікатор телеграм аккаунта',
+        primary_key=True,
+    )
+    first_name = models.CharField(
+        verbose_name="Імя",
+        max_length=50,
+        null=True,
+        blank=True,
+    )
+    last_name = models.CharField(
+        verbose_name="Прізвище",
+        max_length=50,
+        null=True,
+        blank=True,
+    )
+    username = models.CharField(
+        verbose_name="Нікнейм",
+        max_length=50,
+        null=True,
+        blank=True,
+    )
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="tg_account",
+        verbose_name="Користувач",
+    )
