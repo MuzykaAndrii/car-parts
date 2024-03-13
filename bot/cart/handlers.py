3cef3bb84d1af48892426ccb1fa83070848b558a
MuzykaAndrii <andrymyzik@gmail.com>
2024-03-13 22:52:42 +0200

    bot fix: stacking dialogs issue

--- bot/cart/handlers.py
+++ bot/cart/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram_dialog import DialogManager
+from aiogram_dialog import DialogManager, StartMode
 
 from cart.states import CartStates
 from cart.windows import cart_dialog
@@ -12,4 +12,8 @@ router.include_router(cart_dialog)
 
 @router.message(Command("cart"))
 async def command_cart_handler(message: Message, dialog_manager: DialogManager) -> None:
-    await dialog_manager.start(CartStates.cart_detail, data={"user_id": message.from_user.id})
+    await dialog_manager.start(
+        CartStates.cart_detail,
+        mode=StartMode.NEW_STACK,
+        data={"user_id": message.from_user.id}
+    )
--- bot/catalog/handlers.py
+++ bot/catalog/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram_dialog import DialogManager
+from aiogram_dialog import DialogManager, StartMode
 
 from .states import CatalogStates
 from .windows import catalog_dialog
@@ -12,4 +12,4 @@ router.include_router(catalog_dialog)
 
 @router.message(Command("catalog"))
 async def command_catalog_handler(message: Message, dialog_manager: DialogManager) -> None:
-    await dialog_manager.start(CatalogStates.car_providers)
+    await dialog_manager.start(CatalogStates.car_providers, mode=StartMode.NEW_STACK)
